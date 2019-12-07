907e27a90043c37432fd06bfbf5aaa4d8f408106
prasad98765 <pnchaudhari1996@gmail.com>
2019-12-07 17:38:06 +0530

    Addes : store Result in Dictionary

--- ArithmaticOpr.sh
+++ ArithmaticOpr.sh
@@ -5,6 +5,7 @@ read a
 read b
 read c
 
+declare -A allresult
 result=$(( $a + $b * $c ))
 echo $result
  
@@ -16,3 +17,10 @@ echo $result2
 
 result3=$(( $a % $b + $c ))
 echo $result3
+
+allresult[result1]=$result
+allresult[result2]=$result1
+allresult[result3]=$result2
+allresult[result4]=$result3
+
+
